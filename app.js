f95b9db3de1ff843a16f7a152849ba9690a8942a
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-05-25 15:37:49 +0530

    connected mysql database utilities

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./util/db.util.js";
+import { db } from "./util/db.util.js";
 dotenv.config();
 
 const app = express();
@@ -7,6 +9,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+connectDB()
+  .then((res) => {
+    console.log("DB connected", res);
+  })
+  .catch((error) => console.log(error));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
---
+++ util/db.util.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2/promise";
+
+let db;
+
+const connectDB = async function () {
+  db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  console.log(":db connected");
+  const [data, fields] = await db.execute(`select * from products`);
+  console.log(data);
+  console.log("fields", fields);
+};
+
+export { connectDB, db };
