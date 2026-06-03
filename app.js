5809947e663c7f6e0a66b6612078640ed021ca7c
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-06-03 12:03:26 +0530

    added cors functionality

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import { connectDB } from "./util/db.util.js";
 import { db } from "./util/db.util.js";
 import apiRoute from "./route/api.route.js";
+import cors_options from "./service/cors.service.js";
 const app = express();
+app.use(cors(cors_options));
 app.use(express.json());
 app.use(cookieParser());
 
-// middleware
+// mounting routes
 app.use("/api/v1", apiRoute);
 
 app.get("/", (req, res) => {
