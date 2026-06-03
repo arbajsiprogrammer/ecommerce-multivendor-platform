1131c55c00a8e760ecb9fe60bb9a787f19194f22
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-06-03 12:32:08 +0530

    added await keyword

--- util/db.util.js
+++ util/db.util.js
@@ -10,7 +10,7 @@ const connectDB = async function () {
   //   database: process.env.DB_NAME,
   // });
 
-  db = mysql.createPool({
+  db = await mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -26,10 +26,6 @@ const connectDB = async function () {
   });
 
   console.log(":db connected");
-  // const rows = await db.execute(`select * from vendors`);
-  // console.log(rows[0][0].id);
-  // console.log(data);
-  // console.log("fields", fields);
 };
 
 export { connectDB, db };
